3163fcc084afda50c479da35cd3a51040620cf50
Kirill Proskurin <78263182+KirillProgramSite@users.noreply.github.com>
2022-03-13 18:29:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// 1 этап: Прописании логики приложения
+
+let todo = [
+  { id: 1, text: "Купить пиво", checked: false },
+  { id: 2, text: "Купить мясо", checked: false },
+  { id: 3, text: "Купить рыбу", checked: false },
+];
+
+function renderTodo() {
+  if (todo.length > 0) {
+    console.log(todo);
+  } else if (todo.length == 0) {
+    console.log("Нет задач");
+  }
+}
+
+function addTodo(newText) {
+  if (newText) {
+    todo.unshift({ id: 1, text: newText, checked: false });
+    renderTodo();
+  } else {
+    alert("Веддите текст");
+  }
+}
+
+function deleteTodo(pos) {
+  todo.splice(pos, 1);
+  renderTodo();
+}
+
+function checkedTodo(index, value) {
+  if (value == true) {
+    todo[index].checked = true;
+  } else if (value == false) {
+    todo[index].checked = false;
+  }
+  renderTodo();
+}
+
+function allDeleteTodo() {
+    todo.length = 0;
+    renderTodo();
+}
